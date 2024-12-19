2890d9b51bda777daf93f41b920e1f912b95bebd
George Beloshapko <ordcoder@gmail.com>
2024-12-19 07:53:23 +0100

    AoC 2024 19 pt2

--- 2024/19.cpp
+++ 2024/19.cpp
@@ -9,16 +9,16 @@ int main() {
 	copy(istream_iterator<string>(ss), istream_iterator<string>(), back_inserter(towels));
 	int64_t ans = 0;
 	while (cin >> s) {
-		vector<int8_t> reach(s.size() + 1);
-		reach[0] = true;
+		vector<int64_t> reach(s.size() + 1);
+		reach[0] = 1;
 		for (size_t i = 0; i < s.size(); ++i) if (reach[i]) {
 			for (const string &t: towels) {
-				if (i + t.size() <= s.size() && !reach[i + t.size()] && !s.compare(i, t.size(), t)) {
-					reach[i + t.size()] = true;
+				if (i + t.size() <= s.size() && !s.compare(i, t.size(), t)) {
+					reach[i + t.size()] += reach[i];
 				}
 			}
 		}
-		if (reach[s.size()]) ++ans;
+		ans += reach[s.size()];
 	}
 	cout << ans << '\n';
 }
