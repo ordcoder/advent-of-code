5f2fadccf7e600d2f1a3b88f7d86a217c59c7736
George Beloshapko <ordcoder@gmail.com>
2024-12-22 07:42:47 +0100

    AoC 2024 22 pt2

--- 2024/22.cpp
+++ 2024/22.cpp
@@ -2,14 +2,21 @@
 using namespace std;
 
 int main() {
-	uint64_t n, ans = 0;
+	int64_t n, ans = 0;
+	unordered_map<int, int64_t> ban, ana;
 	while (cin >> n) {
+		int p = n % 10, h = 0;
 		for (int t = 0; t < 2000; ++t) {
 			n = (n ^ (n <<  6)) % 16777216;
 			n = (n ^ (n >>  5)) % 16777216;
 			n = (n ^ (n << 11)) % 16777216;
+			h = (h * 20 + n % 10 - p + 10) % 160000;
+			if (t > 2) ban.emplace(h, n % 10);
+			p = n % 10;
 		}
-		ans += n;
+		for (const auto [k, v]: ban) ana[k] += v;
+		ban.clear();
 	}
+	for (const auto [_, v]: ana) ans = max(ans, v);
 	cout << ans << '\n';
 }
