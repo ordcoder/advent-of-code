cb62ac78547e0135879a216e0be334e6a3b407e5
George Beloshapko <ordcoder@gmail.com>
2024-12-23 09:04:50 +0100

    AoC 2024 23 pt2

--- 2024/23.cpp
+++ 2024/23.cpp
@@ -7,21 +7,38 @@ int enc(const char *s) {
 	return 26 * (s[0] - 'a') + s[1] - 'a';
 }
 
+string dec(int e) {
+	return string{char('a' + e / 26), char('a' + e % 26)};
+}
+
+struct Vcomp {
+	bool operator()(const vector<int> &v0, const vector<int> &v1) const {
+		return v0.size() != v1.size() ? v0.size() < v1.size() : v0 < v1;
+	}
+};
+
 int main() {
 	string s;
 	while (cin >> s) {
 		int fr = enc(s.data()), to = enc(s.data() + 3);
 		con[fr][to] = con[to][fr] = true;
 	}
-	int ans = 0;
-	for (int a = 26 * ('t' - 'a'); a < 26 * ('u' - 'a'); ++a) {
-		for (int b = 0; b < 26 * 26; ++b) {
-			if (b / 26 + 'a' == 't' && b <= a) continue;
-			for (int c = b + 1; c < 26 * 26; ++c) {
-				if (c / 26 + 'a' == 't' && c <= a) continue;
-				if (con[a][b] && con[a][c] && con[b][c]) ++ans;
-			}
+	set<vector<int>, Vcomp> comp{vector<int>{}};
+	vector<int> ans;
+	for (auto it = begin(comp); it != end(comp); it = comp.erase(it)) {
+		const vector<int> &c = *it;
+		if (c.size() > ans.size()) ans = c;
+		for (int a = 0; a < 26 * 26; ++a) {
+			bool ok = true;
+			for (int b: c) if (!con[a][b]) { ok = false; break; }
+			if (!ok) continue;
+			vector<int> d(c.size() + 1);
+			copy(begin(c), end(c), begin(d));
+			d.back() = a;
+			sort(begin(d), end(d));
+			comp.emplace(move(d));
 		}
 	}
-	cout << ans << '\n';
+	for (int a: ans) cout << dec(a) << ',';
+	cout << '\n';
 }
