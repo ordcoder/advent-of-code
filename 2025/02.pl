e90738256c5a268e4e9e69e52ed7ea3268ace6b1
George Beloshapko <ordcoder@gmail.com>
2025-12-02 11:44:02 +0100

    AoC 2025 02 pt2

--- 2025/02.pl
+++ 2025/02.pl
@@ -1,23 +1,48 @@
 #!/usr/bin/perl
 use v5.42;
+use List::Util qw(min max);
 my $sum = 0;
 local $/ = ",";
 while (<>) {
 	my ($fr, $to) = split /[-,]/;
-	$sum += sumupto($to) - sumupto($fr-1);
+	$sum += sumall($to) - sumall($fr-1);
 }
 say $sum;
 
-sub sumupto {
+sub sumall {
 	my ($to) = @_;
-	my ($ten, $sum) = (10, 0);
-	$sum += sumrange($ten/10, $ten-1, $ten), $ten*=10 while ($ten*$ten <= $to);
-	my $lim = int($to / ($ten+1));
-	$sum += sumrange($ten/10, $lim, $ten) if ($lim >= $ten/10);
+	return sum2($to) + sum3($to) + sum5($to) + sum7($to);
+}
+
+sub sum2 {
+	my ($to) = @_;
+	my ($ten, $sum) = (1, 0);
+	$sum += sumrange($to, $ten, 10*$ten+1), $ten*=10 while (10*$ten*$ten <= $to);
 	return $sum;
 }
 
+sub sum3 {
+	my ($to) = @_;
+	my ($ten, $sum) = (1, 0);
+	$sum += sumrange($to, $ten, 100*$ten*$ten+10*$ten+1), $ten*=10 while (100*$ten*$ten*$ten <= $to);
+	$sum -= sumrange($to, 1, 111111);
+	return $sum;
+}
+
+sub sum5 {
+	my ($to) = @_;
+	return sumrange($to,  1,      11111) +
+	       sumrange($to, 10,  101010101) -
+	       sumrange($to,  1, 1111111111);
+}
+
+sub sum7 {
+	my ($to) = @_;
+	return sumrange($to, 1, 1111111);
+}
+
 sub sumrange {
-	my ($fr, $to, $ten) = @_;
-	return ($to + $fr) * ($to+1 - $fr) / 2 * ($ten+1);
+	my ($to, $fr, $mult) = @_;
+	my $lim = max(min(int($to / $mult), 10*$fr-1), $fr-1);
+	return ($lim + $fr) * ($lim+1 - $fr) / 2 * $mult;
 }
