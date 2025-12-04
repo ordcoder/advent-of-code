a4c87427c7b3ddc40270bd789f2a6e462abcd348
George Beloshapko <ordcoder@gmail.com>
2025-12-04 08:42:01 +0100

    AoC 2025 04 pt2

--- 2025/04.pl
+++ 2025/04.pl
@@ -1,21 +1,29 @@
 #!/usr/bin/perl
 use v5.42;
-my ($n, $h, $w, @m) = (0, 0, 0);
+my ($n, $r, $h, $w, @m) = (-1, 1, 0, 0);
 for (<>) {
 	push @m, [split //];
 	$w = -1 + length;
 	++$h;
 }
 push @m, [split //, " " x ($w+1)];
-for (my $i = 0; $i < $h; ++$i) {
-	for (my $j = 0; $j < $w; ++$j) {
-		my $c = 0;
-		for (my $y = $i-1; $y <= $i+1; ++$y) {
-			for (my $x = $j-1; $x <= $j+1; ++$x) {
-				++$c if $m[$y][$x] eq "@";
+while ($r) {
+	$n += $r;
+	$r = 0;
+	for (my $i = 0; $i < $h; ++$i) {
+		for (my $j = 0; $j < $w; ++$j) {
+			next unless $m[$i][$j] eq "@";
+			my $c = 0;
+			for (my $y = $i-1; $y <= $i+1; ++$y) {
+				for (my $x = $j-1; $x <= $j+1; ++$x) {
+					++$c if $m[$y][$x] eq "@";
+				}
+			}
+			if ($c <= 4) {
+				$m[$i][$j] = ".";
+				++$r;
 			}
 		}
-		++$n if $m[$i][$j] eq "@" and $c <= 4;
 	}
 }
 say $n;
