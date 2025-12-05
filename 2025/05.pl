99b2905656e4a3c2f46082bfdcbbc5ad6e410fb4
George Beloshapko <ordcoder@gmail.com>
2025-12-05 09:14:27 +0100

    AoC 2025 05 pt2

--- 2025/05.pl
+++ 2025/05.pl
@@ -1,14 +1,17 @@
 #!/usr/bin/perl
 use v5.42;
-my ($n, @r);
+use List::Util qw(max);
+my ($n, $l, @r) = (0, 0);
 while (<>) {
 	my ($f, $t) = split /-/;
 	last unless defined $t;
-	push @r, [$f, $t];
+	push @r, [$f, $t + 1];
 }
-while (<>) {
-	for my $ft (@r) {
-		++$n, last if $ft->[0] <= $_ <= $ft->[1];
+@r = sort {$a->[0] <=> $b->[0] or $a->[1] <=> $b->[1]} @r;
+for my $r (@r) {
+	if ($r->[1] > $l) {
+		$n += $r->[1] - max($r->[0], $l);
+		$l = $r->[1];
 	}
 }
 say $n;
