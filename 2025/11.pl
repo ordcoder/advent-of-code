6c153a8328f8d158d2e74134a1ab55af326193d8
George Beloshapko <ordcoder@gmail.com>
2025-12-11 23:18:23 +0100

    AoC 2025 11 pt1

---
+++ 2025/11.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+use v5.42;
+my (%forw, %back);
+while (<>) {
+	my ($x, @ys) = split /[:\s]+/;
+	for (@ys) {
+		push @{$forw{$x}}, $_;
+		$back{$_}{$x} = 1;
+	}
+}
+my (@q, @top);
+for (keys %forw) {
+	push @q, $_ unless exists $back{$_};
+}
+while (@q) {
+	my $x = shift @q;
+	push @top, $x;
+	for (@{$forw{$x}}) {
+		delete $back{$_}{$x};
+		push @q, $_ unless %{$back{$_}};
+	}
+}
+my %cnt = (you => 1);
+for my $x (@top) {
+	$cnt{$_} += $cnt{$x} // 0 for @{$forw{$x}};
+}
+say $cnt{out};
